055126089167efff4ff6537e39c6b1484447365f
Charles <relate2hazel@gmail.com>
2023-05-25 16:56:27 +0100

    Portfolio brand modification

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -38,3 +38,57 @@ sr.reveal ('.about,.services,.portfolio,.contact',{
 		});
 	}
 )();
+
+// Typing effect
+const carouselText = [
+  {text: " React"},
+  {text: " PHP"},
+  {text: " JavaScript"}
+]
+
+$( document ).ready(async function() {
+  carousel(carouselText, "#typing");
+});
+
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while(i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
+
+async function carousel(carouselList, eleRef) {
+    var i = 0;
+    while(true) {
+      updateFontColor(eleRef, carouselList[i].color)
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(2000);
+      await deleteSentence(eleRef);
+      await waitForMs(200);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
+}
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css('color', color);
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
